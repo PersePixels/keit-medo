c6b757ff7e0b530c678b08075927720c44eb5128
PersePixels <93152976+PersePixels@users.noreply.github.com>
2024-08-29 15:35:06 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version "adamant"
+game 'rdr3'
+lua54 'yes'
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'PersePixels'
+description 'Proximity text chat'
+
+client_scripts {'client.lua'}
+
+shared_scripts {'config.lua'}
+
+server_scripts {'server.lua'}
+
+files({
+	"html/html.html",
+	"html/style.css",
+	"html/HapnaSlabSerif-Medium.ttf",
+	"html/app.js"
+})
+
+ui_page "html/html.html"
