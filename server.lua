249c42fcd910ee1ba3eb0593a93cb5db97112d76
PersePixels <93152976+PersePixels@users.noreply.github.com>
2024-09-09 11:36:07 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,14 +1,14 @@
-RegisterCommand("me", function(source , args , rawCommand)
+RegisterCommand("s", function(source , args , rawCommand)
     args = table.concat(args, ' ')
     TriggerClientEvent('me:event', -1, source, args, 0)
 end)
 
-RegisterCommand("sy", function(source , args , rawCommand)
+RegisterCommand("t", function(source , args , rawCommand)
     args = table.concat(args, ' ')
     TriggerClientEvent('me:event', -1, source, args, 1)
 end)
 
-RegisterCommand("do", function(source , args , rawCommand)
+RegisterCommand("d", function(source , args , rawCommand)
     args = table.concat(args, ' ')
     TriggerClientEvent('me:event', -1, source, args, 2)
 end)
