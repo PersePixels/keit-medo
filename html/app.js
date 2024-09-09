efb69f51836696c3546404f4025312fc1406da8a
PersePixels <93152976+PersePixels@users.noreply.github.com>
2024-09-09 11:34:34 -0700

    Update app.js

--- html/app.js
+++ html/app.js
@@ -1,10 +1,12 @@
 
 addEventListener("message", function(event) {
 
-    if(event.data.meta == "me") {
-		document.getElementById("me").innerHTML = event.data.html;
-	} else if (event.data.meta == "do") {
-		document.getElementById("do").innerHTML = event.data.html;
+    if(event.data.meta == "s") {
+		document.getElementById("s").innerHTML = event.data.html;
+	} else if (event.data.meta == "t") {
+		document.getElementById("t").innerHTML = event.data.html;
+	} else if (event.data.meta == "d") {
+		document.getElementById("d").innerHTML = event.data.html;
 	}
 	
 });
