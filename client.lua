50f8280a764d5b752342781d5a979d3d030d59e9
PersePixels <93152976+PersePixels@users.noreply.github.com>
2024-09-09 11:09:56 -0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,7 @@
 RegisterNetEvent("me:event")
 local players = {}
 local doPlayers = {}
+local syPlayers = {}
 local tRange = 8.0
 local switchRange = false
 local timer = 0
@@ -38,6 +39,43 @@ CreateThread(function()
         end
         if str ~= lstr then
             SendNUIMessage({meta = "me", html = str})
+            lstr = str
+        end
+    end
+end)
+
+CreateThread(function()
+    local lstr = ""
+    while true do
+        Wait(0)
+        local tick = GetGameTimer()
+        local str = ""
+        local removes = {}
+        for a, b in next, syPlayers do
+            if b.rtime < tick then
+                removes[#removes+1] = a
+            end
+            local p = GetPlayerFromServerId(a)
+            if NetworkIsPlayerActive(p) then
+                local ped = GetPlayerPed(p)
+                if DoesEntityExist(ped) then
+                    local coords = GetPedBoneCoords(ped, 0x796e, 0.0, 0.0, 0.0) --HEAD
+                    local ons, x, y = GetHudScreenPositionFromWorldPosition(coords.x, coords.y, coords.z+0.3)
+                    if not ons then
+                        x = (x*100)
+                        y = (y*70)
+                        str = str.."<p style=\"left: "..x.."%;top: "..y.."%;-webkit-transform: translate(-50%, 0%);max-width: 100%;position: fixed;text-align: center;color: #96ff9f;background-color: #000000AA;border-radius:3px; height: 32px;\"><b>⠀"..b.message.."⠀</b></p>"
+                    end
+                end
+            end
+        end
+        if #removes > 0 then
+            for a, b in ipairs(removes) do
+                syPlayers[b] = nil
+            end
+        end
+        if str ~= lstr then
+            SendNUIMessage({meta = "sy", html = str})
             lstr = str
         end
     end
@@ -89,6 +127,8 @@ AddEventHandler("me:event", function(source, message, type1)
         if type1 == 0 then 
             players[source] = {message = fix(message), rtime = 10000+GetGameTimer()} 
         elseif type1 == 1 then 
+            syPlayers[source] = {message = fix(message), rtime = 10000+GetGameTimer()} 
+        elseif type1 == 2 then 
             doPlayers[source] = {message = fix(message), rtime = 10000+GetGameTimer()} 
         end
     end
